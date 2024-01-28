b061043ecbe29ac942126010e237aed274ac4a63
SilentWolfScritps <silentwolfscripts@gmail.com>
2024-01-28 20:44:22 +0100

    init

---
+++ core/processor.py
@@ -0,0 +1,40 @@
+
+import speech_recognition as sr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class IntProcessing:
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+    
+class InputProcessor:
+    
+    def __init__(self) -> None:
+        self.r = sr.Recognizer()
+        pass
+    
+    
+    def AudioInput(file):
+        
+        
+        
